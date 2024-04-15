60a507c1296adbdce230105da1a9b9066f9ff69a
Joe Wong <kcwong.joe@gmail.com>
2024-04-15 17:40:44 +0100

    Fix old link

--- src/win_camera/src/directshow_camera/grabber/ds_grabber_callback.cpp
+++ src/win_camera/src/directshow_camera/grabber/ds_grabber_callback.cpp
@@ -1,7 +1,7 @@
 /**
 * Copy right (c) 2020-2024 Ka Chun Wong. All rights reserved.
 * This is a open source project under MIT license (see LICENSE for details).
-* If you find any bugs, please feel free to report under https://github.com/kcwongjoe/directshow_camera/issues
+* If you find any bugs, please feel free to report under https://github.com/kcwongjoe/win_camera/issues
 **/
 
 #include "directshow_camera/grabber/ds_grabber_callback.h"
